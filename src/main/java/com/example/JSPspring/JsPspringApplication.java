f5c7ad81b1cc71e2812559973b156efa7691a075
N <T50366@intelizign.com>
2023-04-19 13:14:30 +0530

    Added : Spring-Boot Program with Java Server pages(JSP)

---
+++ src/main/java/com/example/JSPspring/JsPspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.JSPspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsPspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsPspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JSPspring/JsPspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JSPspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsPspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
